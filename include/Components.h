40d31719cdf4677726f5e9adc883c21b68c5fb4b
ThatDevShparki <thatdevshparki@gmail.com>
2023-12-29 11:20:35 -0600

    entity implementation

--- include/Components.h
+++ include/Components.h
@@ -5,77 +5,115 @@
 
 class Component
 {
-	public:
-		bool has = false;
+public:
+	bool has = false;
 };
 
 
 class CLifespan : public Component
 {
-	public:
-		int lifespan     = 0;
-		int frameCreated = 0;
+public:
+	int lifespan     = 0;
+	int frameCreated = 0;
 
-		CLifespan() = default;
+	CLifespan() = default;
 
-		CLifespan(int duration, int frame)
-			: lifespan(duration), frameCreated(frame)
-		{
-		};
+	CLifespan(int duration, int frame)
+		: lifespan(duration), frameCreated(frame)
+	{
+	};
+};
+
+class CTransform : public Component
+{
+public:
+	Vec2  pos     = {};
+	Vec2  prevPos = {};
+	Vec2  scale   = { 1.0, 1.0 };
+	Vec2  vel     = {};
+	float angle   = 0;
+
+	CTransform() = default;
+
+	CTransform(const Vec2& p)
+		: pos(p), prevPos(p)
+	{
+	}
+
+	CTransform(const Vec2& p, const Vec2& v, const Vec2& s, float a)
+		: pos(p), prevPos(p), scale(s), vel(v), angle(a)
+	{
+	}
 };
 
 class CBoundingBox : public Component
 {
-	public:
-		Vec2 size     = {};
-		Vec2 halfSize = {};
+public:
+	Vec2 size     = {};
+	Vec2 halfSize = {};
 
-		CBoundingBox() = default;
+	CBoundingBox() = default;
 
-		CBoundingBox(const Vec2& s)
-			: size(s), halfSize(Vec2(s.x / 2, s.y / 2))
-		{
-		}
+	CBoundingBox(const Vec2& s)
+		: size(s), halfSize(Vec2(s.x / 2, s.y / 2))
+	{
+	}
+};
+
+class CInput : public Component
+{
+public:
+	bool up       = false;
+	bool down     = false;
+	bool left     = false;
+	bool right    = false;
+	bool shoot    = false;
+	bool canShoot = true;
+	bool canJump  = true;
+
+	CInput()
+	{
+	};
 };
 
 class CAnimation : public Component
 {
-	public:
-		Animation animation;
-		bool      repeat = false;
+public:
+	Animation animation;
+	bool      repeat = false;
 
-		CAnimation() = default;
+	CAnimation() = default;
 
-		CAnimation(const Animation& animation, bool r)
-			: animation(animation), repeat(r)
-		{
-		}
+	CAnimation(const Animation& animation, bool r)
+		: animation(animation), repeat(r)
+	{
+	}
 };
 
 class CGravity : public Component
 {
-	public:
-		float gravity = 0;
+public:
+	float gravity = 0;
 
-		CGravity() = default;
+	CGravity() = default;
 
-		CGravity(float g)
-			: gravity(g)
-		{
-		}
+	CGravity(float g)
+		: gravity(g)
+	{
+	}
 };
 
 class CState : public Component
 {
-	public:
-		std::string state;
+public:
+	std::string state;
 
-		CState() = default;
+	CState() = default;
 
-		CState(const std::string& s)
-			: state(s)
-		{
-		}
+	CState(const std::string& s)
+		: state(s)
+	{
+	}
 };
 
 
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,1 +1,35 @@
 #include "Entity.h"
+
+Entity::Entity(const size_t id, const std::string& tag)
+	: m_id(id), m_tag(tag)
+{
+	m_components = std::make_tuple(
+		CTransform(),
+		CLifespan(),
+		CInput(),
+		CBoundingBox(),
+		CAnimation(),
+		CGravity(),
+		CState()
+	);
+}
+
+bool Entity::isActive() const
+{
+	return m_active;
+}
+
+const std::string& Entity::tag() const
+{
+	return m_tag;
+}
+
+size_t Entity::id() const
+{
+	return m_id;
+};
+
+void Entity::destroy()
+{
+	m_active = false;
+}
